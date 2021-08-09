ed86f812dfc0408e34370abba30d7668d18ea7e5
Liam Neeson <shabekuri7@naver.com>
2021-08-09 16:51:03 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by TAEKMIN LEE on 2021/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
